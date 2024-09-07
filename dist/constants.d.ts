3ea066632318d14fc90b38f77b295e1f55e333b3
kkkkk-npc <kkkkknpc@proton.me>
2024-09-07 16:49:09 +0800

    feat: display dist file

---
+++ dist/constants.d.ts
@@ -0,0 +1,9 @@
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0x1465B035F63Db74f1eA714e7509C92913081A74c";
+export declare const INIT_CODE_HASH = "0x47f06d9eb0286e1f1f9ee798bc0b6a50e8b8ba6741a4d7a6748dbed280d62a5e";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
